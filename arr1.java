6f64ee01d9b5fbb0f8bb9f0fb36a7ebc05f061af
parvathi142003 <parvathi142003@gmail.com>
2024-11-05 15:59:45 +0530

    day 1

---
+++ arr1.java
@@ -0,0 +1,10 @@
+class loops
+{
+ public static void main(String args[])
+ {
+  loops l= new loops();
+  System.out.println(l.get class());
+  int arr[]=new int[5];
+  System.out.println(arr.get class());
+ }
+}
